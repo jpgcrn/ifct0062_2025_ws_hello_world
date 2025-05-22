0a79b3f4dd24f6adfe235d5343e5a18dc2a45769
jpgcrn <puiguijaindustrial@gmail.com>
2025-05-22 19:47:59 +0200

    little change
    para probar el despliegue en Jenkins

--- src/main/java/com/crngetafe/ifct0062_2025_ws_hello_world/controller/SaludadorController.java
+++ src/main/java/com/crngetafe/ifct0062_2025_ws_hello_world/controller/SaludadorController.java
@@ -9,7 +9,7 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class SaludadorController {
-    private static final String template = "Hola, %s!";
+    private static final String template = "Holita, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/saludo")
